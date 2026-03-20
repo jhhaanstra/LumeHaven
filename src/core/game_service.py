ac48ab16e2215ea487a7eeade9f98124ca1b375b
Jan Hendrik Haanstra <janhendrikhaanstra@gmail.com>
2026-03-20 21:34:34 +0100

    ref: refactored event publisher to subscriber pattern

--- src/core/game_service.py
+++ src/core/game_service.py
@@ -20,6 +20,7 @@ from src.core.events import (
     CharacterReceivedDamage,
     CharacterGainedExperience,
     MonsterReceivedDamage,
+    Event,
 )
 from src.ghs.client import GameStateFetcher
 from src.core.config import Config
@@ -46,26 +47,45 @@ class GameService:
     ]
 
     def __init__(self, event_publisher: EventPublisher, interval_ms: int):
-        self.game_state_fetcher = None
-        self.current_state: GameState = None
-        self.scheduler = BackgroundScheduler()
-        self.scheduler.add_job(event_publisher.run, "interval", seconds=interval_ms / 1000)
+        self._game_state_fetcher = None
+        self._current_state: GameState = None
+        self._scheduler = BackgroundScheduler()
+        self._scheduler.add_job(event_publisher.publish_events, "interval", seconds=interval_ms / 1000)
 
     def start(self):
-        self.scheduler.start()
+        self._scheduler.start()
 
     def stop(self):
-        self.scheduler.pause()
+        self._scheduler.pause()
 
     def is_started(self) -> bool:
-        return self.scheduler.state == STATE_RUNNING
+        return self._scheduler.state == STATE_RUNNING
 
 
 class EventPublisher(ABC):
 
+    _subscribers: list[EventSubScriber] = []
+
     @abstractmethod
-    def run(self):
+    def _check_events(self) -> list[Event]:
         pass
+
+    def publish_events(self):
+        for event in self._check_events():
+            logging.info(f"Event triggered: {event.__class__.__name__}")
+            for subscriber in self._subscribers:
+                subscriber.on_event(event)
+
+    def subscribe(self, subscriber: EventSubScriber):
+        self._subscribers.append(subscriber)
+
+
+class EventSubScriber(ABC):
+
+    @abstractmethod
+    def on_event(self, event: Event):
+        pass
+
 
 class DbReadingEventPublisher(EventPublisher):
     events = [
@@ -90,23 +110,22 @@ class DbReadingEventPublisher(EventPublisher):
         return DbReadingEventPublisher(config.ghs.sqlite_db, config.ghs.game_code)
 
     def __init__(self, sqlite_db: str, game_code: str):
-        self.game_state_fetcher = None
-        self.current_state: GameState = None
-        self.sqlite_db = sqlite_db
-        self.game_code = game_code
+        self._game_state_fetcher = None
+        self._current_state: GameState = None
+        self._sqlite_db = sqlite_db
+        self._game_code = game_code
 
-    def run(self):
-        if not self.game_state_fetcher:
-            self.game_state_fetcher = GameStateFetcher(self.sqlite_db, self.game_code)
-            self.current_state = self.game_state_fetcher.fetch_game_state()
+    def _check_events(self) -> list[Event]:
+        if not self._game_state_fetcher:
+            self._game_state_fetcher = GameStateFetcher(self._sqlite_db, self._game_code)
+            self._current_state = self._game_state_fetcher.fetch_game_state()
+            return []
         else:
-            new_state = self.game_state_fetcher.fetch_game_state()
+            new_state = self._game_state_fetcher.fetch_game_state()
             logging.debug(f"State fetched: {str(new_state)}")
-            matching_events = self._check_for_events(self.current_state, new_state)
-            for event in matching_events:
-                logging.info(f"Event triggered: {event.__class__.__name__}")
-
-            self.current_state = new_state
+            matching_events = self._check_for_events(self._current_state, new_state)
+            self._current_state = new_state
+            return matching_events
 
     def _check_for_events(self, old_game_state: GameState, new_game_state: GameState):
         return [event for event in self.events if event.matches(old_game_state, new_game_state)]
--- src/core/test/game_service_test.py
+++ src/core/test/game_service_test.py
@@ -1,11 +1,12 @@
 import unittest
 
-from src.core.game_service import GameService, EventPublisher
+from src.core.events import Event, FireElementActive
+from src.core.game_service import GameService, EventPublisher, EventSubScriber
 
 
 class NoOpEventPublisher(EventPublisher):
 
-    def run(self):
+    def _check_events(self) -> list[Event]:
         pass
 
 
@@ -18,3 +19,32 @@ class TestGameService(unittest.TestCase):
         self.assertTrue(game_service.is_started())
         game_service.stop()
         self.assertFalse(game_service.is_started())
+
+
+class StaticEventPublisher(EventPublisher):
+
+    events_to_publish = []
+
+    def _check_events(self) -> list[Event]:
+        events_copy = self.events_to_publish.copy()
+        self.events_to_publish.clear()
+        return events_copy
+
+class StaticEventSubscriber(EventSubScriber):
+
+    received_events = []
+
+    def on_event(self, event: Event):
+        self.received_events.append(event)
+
+
+class TestEventPublisher(unittest.TestCase):
+
+    def test_should_publish_to_subscribers(self):
+        publisher = StaticEventPublisher()
+        subscriber = StaticEventSubscriber()
+        publisher.subscribe(subscriber)
+        event = FireElementActive()
+        publisher.events_to_publish.append(event)
+        publisher.publish_events()
+        self.assertEqual(subscriber.received_events, [event])
